736ab5a98673fcf4e71ed973b3526b9aaeef3f4f
chiraponkub <52907033+chiraponkub@users.noreply.github.com>
2019-07-15 14:43:19 +0700

    Manager
    BELL

---
+++ ITUTCSQLSERVER/ITUTCSQLSERVER/Manager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ITUTCSQLSERVER
+{
+    class Manager
+    {
+        public static void Query(string Command)
+        {
+            Database.Connection.Open();
+            SqlCommand cmd = new SqlCommand(Command, Database.Connection);
+            cmd.ExecuteNonQuery();
+            Database.Connection.Close();
+        }
+        public static void Insert(string Var1, string Var2)
+        {
+            try
+            {
+                Database.Connection.Open();
+                string sqlCommand = @"INSER INTO table() VALUES()";
+                SqlCommand Command = new SqlCommand(sqlCommand, Database.Connection);
+                Command.Prepare();
+                Command.Parameters.AddWithValue("@var1", Var1);
+                Command.Parameters.AddWithValue("@var2", Var2);
+                Command.ExecuteNonQuery();
+            }
+            finally
+            {
+                Database.Connection.Close();
+            }
+        }
+        public static void Update(string Var1, string Var2)
+        {
+            try
+            {
+                Database.Connection.Open();
+                string sqlCommand = "UPDATE table SET Field=@field WHERE field_id='" + Var1 + Var2;
+                SqlCommand Command = new SqlCommand(sqlCommand, Database.Connection);
+                Command.Prepare();
+                Command.Parameters.AddWithValue("@var1", Var1);
+                Command.Parameters.AddWithValue("@var2", Var2);
+                Command.ExecuteNonQuery();
+            }
+            finally
+            {
+                Database.Connection.Close();
+            }
+        }
+        public static void Delete(string VarID)
+        {
+            try
+            {
+                Database.Connection.Open();
+                string sqlCommand = @"DELETE FROM table WHERE VarID=@VarID";
+                SqlCommand Command = new SqlCommand(sqlCommand, Database.Connection);
+                Command.Prepare();
+                Command.Parameters.AddWithValue("@StudentID", VarID);
+                Command.ExecuteNonQuery();
+            }
+            finally
+            {
+                Database.Connection.Close();
+            }
+        }
+    }
+}
