1ba482f3f10b3ed128ae2d4ba047a79081c92a06
Mr.Partchayanan <Partchayanan.y@softubon.com>
2019-07-15 13:02:32 +0700

    Cimmit

--- ITUTCSQLSERVER/ITUTCSQLSERVER/Main.Designer.cs
+++ ITUTCSQLSERVER/ITUTCSQLSERVER/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ITUTCSQLSERVER
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Page";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
