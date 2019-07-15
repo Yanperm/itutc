0f336b802d504372f36fe2a3e9eb172e83216049
wassanawisakans <wassanawisakan098106@gmail.com>
2019-07-15 15:14:29 +0700

    ิิิddd

--- ITUTCSQLSERVER/ITUTCSQLSERVER/Activity.Designer.cs
+++ ITUTCSQLSERVER/ITUTCSQLSERVER/Activity.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.manageDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,7 +40,7 @@
             this.manageDatabaseToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(306, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(350, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -49,11 +50,21 @@
             this.manageDatabaseToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
             this.manageDatabaseToolStripMenuItem.Text = "Manage Database";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Activity
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(306, 186);
+            this.ClientSize = new System.Drawing.Size(350, 256);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Activity";
@@ -69,5 +80,6 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem manageDatabaseToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
