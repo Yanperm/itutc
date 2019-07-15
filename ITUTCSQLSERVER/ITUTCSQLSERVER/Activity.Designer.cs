ee5ec6c8b215b420bbda996156f0fda28e0915e1
Mr.Partchayanan <Partchayanan.y@softubon.com>
2019-07-15 14:00:48 +0700

    สร้างฟอร์มกิจกรรม

---
+++ ITUTCSQLSERVER/ITUTCSQLSERVER/Activity.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ITUTCSQLSERVER
+{
+    partial class Activity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Activity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 280);
+            this.Name = "Activity";
+            this.Text = "Activity";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
