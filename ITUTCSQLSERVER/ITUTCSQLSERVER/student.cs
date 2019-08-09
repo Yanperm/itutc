8de19b2bb7b613e3b241734edd137cdbf15130a2
fair1998 <52907026+fair1998@users.noreply.github.com>
2019-08-09 14:33:47 +0700

    จ๋า

--- ITUTCSQLSERVER/ITUTCSQLSERVER/student.cs
+++ ITUTCSQLSERVER/ITUTCSQLSERVER/student.cs
@@ -19,16 +19,20 @@ namespace ITUTCSQLSERVER
 
         private void lebel1_Click(object sender, EventArgs e)
         {
+            //String STUDENT_NAME = txtSTUDENT_NAME.Text;
+           // String STUDENT_LASTNAME = txtSTUDENT_LASTNAME.Text;
+            //String STUDENT_TEL = txtSTUDENT_TEL.Text;
+            //Manager.InsertStudent(STUDENT_NAME, STUDENT_LASTNAME, STUDENT_TEL);
+            //MessageBox.Show("บันทึก", "แจ้งเตือน");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
             String STUDENT_NAME = txtSTUDENT_NAME.Text;
             String STUDENT_LASTNAME = txtSTUDENT_LASTNAME.Text;
             String STUDENT_TEL = txtSTUDENT_TEL.Text;
             Manager.InsertStudent(STUDENT_NAME, STUDENT_LASTNAME, STUDENT_TEL);
             MessageBox.Show("บันทึก", "แจ้งเตือน");
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void Button1_MouseClick(object sender, MouseEventArgs e)
